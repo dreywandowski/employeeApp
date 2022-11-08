e98650617f67df0d8eb5e8027af09a4be7d48f5a
Aduramimo <Aduramimo@aduramimo.qjdbwnx25byunmkxzwfzzur0oh.dx.internal.cloudapp.net>
2022-11-08 15:29:37 +0000

    verify JWT token

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,15 +2,16 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var admin = require('../controllers/AdminController');
+var auth = require('../middleware/verifyToken');
 
  // delete employee
 router.delete('/remove_employe/:name', admin.deleteEmployee);
 
  // get employees
- router.get('/employees', admin.getEmployees);
+ router.get('/employees', auth,admin.getEmployees);
 
   // get employee
-  router.get('/employees/:name', admin.employee_id);
+  router.get('/employees/:name', [auth], admin.employee_id);
 
 
 
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var employee = require('../controllers/EmployeeController');
+var auth = require('../middleware/verifyToken');
 //var env = require('./connection');
 
 
@@ -12,11 +13,13 @@ const {body, validationResult} = require('express-validator');
 var testJson = (req, res, next) => {
     const content = req.headers['content-type'];
     console.log("test Json request header is =="+content);
+    //res.status(401).json({ error: 'token crlkrsss', status : 0 })
    /* if (request.headers['content-type'] !== 'application/json') {
         response.status(400).send('Server requires application/json')
     } else {
       
     }*/
+
     next()
    }
       // index
@@ -28,14 +31,18 @@ var testJson = (req, res, next) => {
       // login
     router.post('/login',  urlencodedParser, employee.login);
 
-    // edit profile
-    router.put('/update_employee/:name',  urlencodedParser, employee.editProfile);
-
     // verify token
-   // app.post("/verifyToken", urlencodedParser, employee.verify);
+    router.get('/verifyToken', [testJson, auth], employee.verify);
+
+    // edit profile
+    //router.put('/update_employee/:name',  [urlencodedParser, auth], employee.editProfile);
+
+    
+
+  
 
      // logout
-     router.post('/logout',  urlencodedParser, employee.logout);
+     router.post('/logout',  auth, employee.logout);
 
 
 module.exports = router;

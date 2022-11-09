8dc85dace7acdafef6747085b2ae425049226f1c
Aduramimo <Aduramimo@aduramimo.qjdbwnx25byunmkxzwfzzur0oh.dx.internal.cloudapp.net>
2022-11-09 17:14:43 +0000

    fixed admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,15 +3,30 @@ var app = express();
 var router = express.Router();
 var admin = require('../controllers/AdminController');
 var auth = require('../middleware/verifyToken');
+var adminVerify = require('../middleware/verifyAdmin');
+
+// verify user if its admin
+/*var adminVerify = (req, res, next) => {
+  const role = req.user.role;
+
+  if(role === "admin"){
+    next()
+  }
+  else{
+    res.status(404).json({'message' : 'This user is not authorized to use this route!', 
+            'status': 0});
+  }
+  
+ }*/
 
  // delete employee
-router.delete('/remove_employe/:name', admin.deleteEmployee);
+router.delete('/remove_employe/:name',[auth, adminVerify], admin.deleteEmployee);
 
  // get employees
- router.get('/employees', auth,admin.getEmployees);
+ router.get('/employees', [auth, adminVerify], admin.getEmployees);
 
   // get employee
-  router.get('/employees/:name', [auth], admin.employee_id);
+  router.get('/employees/:name', [auth, adminVerify], admin.employee_id);
 
 
 

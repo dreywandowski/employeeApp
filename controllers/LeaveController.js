6315c6693793599e74d599bb393cb1daaadf914a
Aduramimo <Aduramimo@aduramimo.qjdbwnx25byunmkxzwfzzur0oh.dx.internal.cloudapp.net>
2022-11-12 17:00:26 +0000

    mail sent for approved/rejected leave requests

--- controllers/LeaveController.js
+++ controllers/LeaveController.js
@@ -6,11 +6,12 @@ const nodemailer = require('nodemailer');
 
 var eventEmitter = new EventEmitter();
 
+// send mail to admin for approval/rejection
 eventEmitter.on('sendMail', (msg) => {
     const sendEmail = (receiver, subject, content) => {
          user = process.env.MAIL_USERNAME;
          pass = process.env.MAIL_PWD;
-         
+
         let transport = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
@@ -42,8 +43,47 @@ eventEmitter.on('sendMail', (msg) => {
    });
 });
 }
-
         sendEmail("aduramimo@gmail.com", "Leave requests awaiting your approval");
+ });
+
+ // send mail to the user for approval/rejection
+ eventEmitter.on('sendFeedbackMail', (msg) => {
+    const sendEmail = (receiver, subject, content) => {
+         user = process.env.MAIL_USERNAME;
+         pass = process.env.MAIL_PWD;
+
+        let transport = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: user,
+          pass: pass
+        }
+     });
+
+     ejs.renderFile('/var/www/html/payroll/assets/templates/leaves_approved_rejected.ejs', {content:msg}, (err, data) => {
+        if (err) {
+          console.log("error opening the file!! "+err);
+        } else {
+          var mailOptions = {
+            from: 'admin@employeeapp.com',
+            to: 'aduramimo@gmail.com',
+            subject: subject,
+            html: data
+          };
+        }
+   
+   transport.sendMail(mailOptions, function(err, info) {
+       if (err) {
+         console.log("cant send mail!! " + err);
+       } else {
+         console.log("mail sent ok "+"content=="+ msg+info);
+       }
+   });
+});
+}
+        sendEmail("aduramimo@gmail.com", "Update on Your Leave Application");
  });
 
 // get all leaves
@@ -162,6 +202,7 @@ const getLeavesApproval = (req, res) => {
 // approve leave request -- admin (supervisors) only
 const approveLeave = (req, res) => {
     leave.update({approved: 1, approved_by: req.user.user}, {where: {id: req.params.id}} ).then(leave => {
+        eventEmitter.emit('sendFeedbackMail', 1);
         res.status(200).json({'message' : 'Leave request approved sucessfully!', 
                           'status':1});
     }).catch(err =>{
@@ -173,6 +214,7 @@ const approveLeave = (req, res) => {
 // reject leave request -- admin (supervisors) only
 const rejectLeave = (req, res) => {
     leave.update({status: 'cancelled', approved: 0, rejected_by: req.user.user}, {where: {id: req.params.id}} ).then(leave => {
+        eventEmitter.emit('sendFeedbackMail', 0);
         res.status(200).json({'message' : 'Leave request rejected sucessfully!', 
                           'status':1});
     }).catch(err =>{

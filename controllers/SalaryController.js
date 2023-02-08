dc59364f8df409de9046a8a6780546c93d772cd8
Aduramimo <Aduramimo@aduramimo.qjdbwnx25byunmkxzwfzzur0oh.dx.internal.cloudapp.net>
2023-02-08 18:05:38 +0000

    breakdown

--- controllers/SalaryController.js
+++ controllers/SalaryController.js
@@ -1,16 +1,15 @@
 var users = require('../models/Users');
+const pdfMaster = require("pdf-master");
 
-let basic_salary = 70000;  let hmo = 50000; let housing = 7000; let furniture = 6500; 
- let leave = 40000; let dressing = 30000; let transport = 9000; let education = 8000; 
- let utility = 30000;
+const salUtil = (username, caller) => {
+    let basic_salary = 70000;  let hmo = 50000; let housing = 7000; let furniture = 6500; 
+    let leave = 40000; let dressing = 30000; let transport = 9000; let education = 8000; 
+    let utility = 30000;
+   
+    const allowed_grades = ["ABO", "SET", "EXE", "RED"]; 
+    
 
- const allowed_grades = ["ABO", "SET", "EXE", "RED"];
-
- 
-// salary calculation
-const calculateSalary = (req, res) => {
-    let username = req.params.name;
-users.findOne({where: {username: username}, 
+    users.findOne({where: {username: username}, 
         attributes: ['username', 'firstName', 'lastName','rank']}).
          then(resp =>{
             if(allowed_grades.includes(resp.rank)){
@@ -42,22 +41,46 @@ users.findOne({where: {username: username},
                                     default:
 
                 }
-                const salary = basic_salary + hmo + housing + furniture + leave + dressing + transport + education + utility;
-
-                res.status(200).json({'message' : 'Salary computed successfully!', 
-                'salary': salary, 'status': 1});
+                var salary = basic_salary + hmo + housing + furniture + leave + dressing + transport + education + utility;
+                var breakdown = [basic_salary, hmo, housing, furniture, leave, dressing,transport, education,utility];
+              
+                 if(caller == 'calc'){
+                    console.log('res === '+salary);
+                    return salary;
+                }
+                else if (caller == 'breakdown'){
+                    return breakdown;
+                }
+                else{
+                    return "";
+                }
             }
                else{
-                throw new Error('Not among approved grades!!');
+                return 'Not among approved grades!!';
                }
-            }).
-        catch(err =>{
-            res.status(404).json({'message' : 'Unable to set up salary calulation', 
-            'error': " "+err, 'status': 0});
-        });
+            });
 
-
+       
 }
+
+ 
+// salary calculation
+const calculateSalary = (req, res) => {
+    let username = req.params.name;
+   const sal = salUtil(username, "calc");
+        console.log("hereee  at caller "+sal);
+
+        if(sal != -1){
+         res.status(200).json({'message' : 'Salary computed successfully!', 
+         'salary': sal, 'status': 1});
+        }
+     else{
+     res.status(404).json({'message' : 'Unable to set up salary calulation',  'status': 0});
+ }
+   
+        };
+
+    
 // retrive my salary calculation/breakdown
 const mySalaryBreakDown = (req, res) => {
     let username = req.params.name;
@@ -111,7 +134,7 @@ const mySalaryBreakDown = (req, res) => {
 // convert my payroll breakdown to PDF
 
 const salaryToPdf = (req, res) =>{
-    
+
 }
 
 module.exports = {

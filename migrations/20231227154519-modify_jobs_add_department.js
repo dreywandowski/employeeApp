e5de8084e44a82f268a50c38924a96a28ff98a24
Oludare Aduramimo <oaduramimo@renmoney.com>
2024-01-02 17:33:49 +0100

    new migration

--- migrations/20231227154519-modify_jobs_add_department.js
+++ migrations/20231227154519-modify_jobs_add_department.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     queryInterface.addColumn(
-      'jobs', 
+      'Jobs', 
       'department', 
       {
         type: Sequelize.STRING,
